50b452f7093f37df0033fe7e2384aa55c59e9a8e
gustblima <gustavolima320@gmail.com>
2020-08-30 20:19:56 -0300

    Add initial backend CI

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,7 +7,21 @@ import { Connection } from 'typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.TYPEORM_HOST || "localhost",
+      port: 3306,
+      username: process.env.TYPEORM_USERNAME || "root",
+      password: process.env.TYPEORM_PASSWORD || "password",
+      database: "catalogue",
+      entities: ["dist/**/**.entity{.ts,.js}"],
+      migrations: [
+        "dist/migrations/**/*.js"
+      ],
+      logging: true,
+      migrationsRun: true,
+      synchronize: true
+    }),
     ProductModule
   ],
   controllers: [AppController],
